c216866e4d5af9eabc3a5bf5b5210bb8b4fc9393
ginpaydo <y-toyoda@live.jp>
2020-01-28 23:43:41 +0900

    ボタンを追加するために。

--- RensyuRensyu/ClientApp/src/components/leaflet/NazoMap.tsx
+++ RensyuRensyu/ClientApp/src/components/leaflet/NazoMap.tsx
@@ -14,22 +14,41 @@ type NazoMapProps =
 
 /**
  * 地図を定義します。
+ * 基本的にここを見ましょう
+ * https://react-leaflet.js.org/docs/en/examples
  */
 class NazoMap extends React.PureComponent<NazoMapProps> {
     public render() {
         return (
             <Map center={[45.4, -75.7]} zoom={13}>
+                {/* 地図を提供しているサービスから画像を貰う、どこから貰っているかは右下に表示される */}
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                {/* 見えないけど、クリックしたらメッセージを吹き出し表示するようになる */}
                 <Marker position={[45.4, -75.7]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
                 </Marker>
+
+
+                            
+                                            
             </Map>
         );
+              //  L.easyButton( 'fa-gbp', function(){
+              //      zoomTo.setView([55, -2], 4);
+              //}).addTo(zoomTo);
+
+              //  L.easyButton( 'fa-jpy', function(){
+              //      zoomTo.setView([38, 139], 4);
+              //}).addTo(zoomTo);
+
+              //  L.easyButton( 'fa-usd', function(){
+              //      zoomTo.setView([37.8, -96], 3);
+              //}).addTo(zoomTo);
     }
 } 
 
