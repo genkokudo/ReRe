a3c702aaf59520543bd494243e7c5cd84ab5a877
ginpaydo <y-toyoda@live.jp>
2020-02-16 21:48:01 +0900

    マーカーにfontawesomeを使用

--- RensyuRensyu/ClientApp/src/components/leaflet/NazoMap.tsx
+++ RensyuRensyu/ClientApp/src/components/leaflet/NazoMap.tsx
@@ -1,10 +1,13 @@
 ﻿import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import Control from 'react-leaflet-control';
 import NazoMapModule from "../../store/leaflet/NazoMap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { divIcon } from 'leaflet';
+import { renderToStaticMarkup } from 'react-dom/server';
 
+import '../../leaflet.css'
 //import L from 'leaflet'
 
 /*
@@ -21,18 +24,26 @@ const NazoMap = () => {
     //// store の state を取得する
     //const count = useSelector((state: any) => state.nazoMap.count);
 
+    // MarkerにFontawesomeを使用
+    const iconMarkup = renderToStaticMarkup(<FontAwesomeIcon icon={['fas', 'minus-circle']} />);
+    const customMarkerIcon = divIcon({
+        html: iconMarkup,
+    });
+
     // 描画処理
     return (
         <React.Fragment>
-            <Map center={[45.4, -75.7]} zoom={13}>
+            <Map center={[34.96841, 136.62732]} zoom={13}>
                 {/* 地図を提供しているサービスから画像を貰う、どこから貰っているかは右下に表示される */}
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                {/* 見えないけど、クリックしたらメッセージを吹き出し表示するようになる */}
-                <Marker position={[45.4, -75.7]}>
+                {/* クリックしたらメッセージを吹き出し表示するようになる */}
+                {/* TODO:白い四角形消したい */}
+                <Marker position={[34.96841, 136.62732]} onClick={() => alert('マーカーのクリック')} icon={customMarkerIcon}>
                     <Popup>
+                        <FontAwesomeIcon icon={['fas', 'minus-circle']} />
                         ポップアップができるよ！ <br /> 改行もできるよ！
                     </Popup>
                 </Marker>
@@ -46,6 +57,27 @@ const NazoMap = () => {
                         <div><FontAwesomeIcon icon={['fab', 'github']} />アラート</div>
                     </button>
                 </Control>
+                <Control position="topleft" >
+                    <button onClick={() => alert('alert')}>
+                        <div><FontAwesomeIcon icon={['fab', 'github']} />アラート</div>
+                    </button>
+                </Control>
+
+                {/* 線を引く */}
+                <Polyline positions={[
+                    [35.007904, 136.597519],
+                    [35.02664, 136.622259],
+                    [34.982158, 136.631795],
+                    [34.965716, 136.614922],
+                    [34.970725, 136.555688],
+                    [34.99368, 136.542041],
+                    [35.032062, 136.562559],
+                    [35.009322, 136.577461],
+                    [35.007904, 136.597519]
+                ]}>
+                </Polyline>
+                <Polyline positions={[[34.997932, 136.600816], [34.991654, 136.599221]]} />
+                <Polyline positions={[[34.999107, 136.572963], [34.991705, 136.569218]]} />
             </Map>
         </React.Fragment>
     );
